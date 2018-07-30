41a8ca7453e6b0e83f8928b75092ec649e140eaf
Graham <mogmog@gmail.com>
2018-07-30 10:35:51 +0100

    added headlines

--- src/components/Admin/HeadlineCreator.js
+++ src/components/Admin/HeadlineCreator.js
@@ -1,123 +1,113 @@
 import React, {Component} from 'react'
 import {render} from 'react-dom' // eslint-disable-line no-unused-vars
-import Editor, { composeDecorators } from 'draft-js-plugins-editor'
+import Editor, {composeDecorators} from 'draft-js-plugins-editor'
 import {EditorState, convertFromRaw, convertToRaw} from 'draft-js'
 import {fromJS} from 'immutable';
 import createMentionPlugin, {defaultSuggestionsFilter} from '../EditorPlugins/draft-js-mention-plugin/src';
 import createImagePlugin from 'draft-js-image-plugin';
+
+import createToolbarPlugin, { Separator } from 'draft-js-static-toolbar-plugin';
+import {
+  ItalicButton,
+  BoldButton,
+  UnderlineButton,
+  CodeButton,
+  HeadlineOneButton,
+  HeadlineTwoButton,
+  HeadlineThreeButton,
+  UnorderedListButton,
+  OrderedListButton,
+  BlockquoteButton,
+  CodeBlockButton,
+} from 'draft-js-buttons';
+
+
+const text = 'In this editor a toolbar shows up once you select part of the text …';
+
+
 import {raw} from './initialState/raw';
 import Search from './Plugins/Search';
 
 import definitionComponent from './Plugins/Definition/Component';
-import DefinitionEntry     from './Plugins/Definition/Entry';
+import DefinitionEntry from './Plugins/Definition/Entry';
 
 import contentComponent from './Plugins/Content/Component';
-import ContentEntry     from './Plugins/Content/Entry';
+import ContentEntry from './Plugins/Content/Entry';
 
-import regionComponent  from './Plugins/Region/Component';
-import RegionEntry      from './Plugins/Region/Entry';
+import regionComponent from './Plugins/Region/Component';
+import RegionEntry from './Plugins/Region/Entry';
 
 import heatmapComponent from './Plugins/Heatmaps/Component';
-import HeatmapEntry     from './Plugins/Heatmaps/Entry';
+import HeatmapEntry from './Plugins/Heatmaps/Entry';
 
 import graphComponent from './Plugins/Graph/Component';
-import GraphEntry     from './Plugins/Graph/Entry';
+import GraphEntry from './Plugins/Graph/Entry';
 
 
 
-import styles from './styles.css';
-
-class ImageAdd extends Component {
-  // Start the popover closed
-  state = {
-    url: '',
-    open: false,
-  };
-
-  // When the popover is open and users click anywhere on the page,
-  // the popover should close
+class HeadlinesPicker extends Component {
   componentDidMount() {
-    document.addEventListener('click', this.closePopover);
+    setTimeout(() => { window.addEventListener('click', this.onWindowClick); });
   }
 
   componentWillUnmount() {
-    document.removeEventListener('click', this.closePopover);
+    window.removeEventListener('click', this.onWindowClick);
   }
 
-  // Note: make sure whenever a click happens within the popover it is not closed
-  onPopoverClick = () => {
-    this.preventNextClose = true;
-  }
-
-  openPopover = () => {
-    if (!this.state.open) {
-      this.preventNextClose = true;
-      this.setState({
-        open: true,
-      });
-    }
-  };
-
-  closePopover = () => {
-    if (!this.preventNextClose && this.state.open) {
-      this.setState({
-        open: false,
-      });
-    }
-
-    this.preventNextClose = false;
-  };
-
-  addImage = () => {
-    const { editorState, onChange } = this.props;
-    onChange(this.props.modifier(editorState, this.state.url));
-  };
-
-  changeUrl = (evt) => {
-    this.setState({ url: evt.target.value });
-  }
+  onWindowClick = () =>
+    // Call `onOverrideContent` again with `undefined`
+    // so the toolbar can show its regular content again.
+    this.props.onOverrideContent(undefined);
 
   render() {
-    const popoverClassName = this.state.open ?
-      styles.addImagePopover :
-      styles.addImageClosedPopover;
-    const buttonClassName = this.state.open ?
-      styles.addImagePressedButton :
-      styles.addImageButton;
-
+    const buttons = [HeadlineOneButton, HeadlineTwoButton, HeadlineThreeButton];
     return (
-      <div className={styles.addImage}>
-        <button
-          className={buttonClassName}
-          onMouseUp={this.openPopover}
-          type="button"
-        >
-          +
-        </button>
-        <div
-          className={popoverClassName}
-          onClick={this.onPopoverClick}
-        >
-          <input
-            type="text"
-            placeholder="Paste the image url …"
-            className={styles.addImageInput}
-            onChange={this.changeUrl}
-            value={this.state.url}
-          />
-          <button
-            className={styles.addImageConfirmButton}
-            type="button"
-            onClick={this.addImage}
-          >
-            Add
-          </button>
-        </div>
+      <div>
+        {buttons.map((Button, i) => // eslint-disable-next-line
+          <Button key={i} {...this.props} />
+        )}
       </div>
     );
   }
 }
 
+class HeadlinesButton extends Component {
+  onClick = () =>
+    // A button can call `onOverrideContent` to replace the content
+    // of the toolbar. This can be useful for displaying sub
+    // menus or requesting additional information from the user.
+    this.props.onOverrideContent(HeadlinesPicker);
+
+  render() {
+    return (
+      <div className={buttonStyles.buttonWrapper}>
+        <button onClick={this.onClick} className={buttonStyles.button} >
+          H
+        </button>
+      </div>
+    );
+  }
+}
+
+import editorStyles from './editorStyles.less';
+import buttonStyles from './buttonStyles.less';
+import toolbarStyles from './toolbarStyles.less';
+
+const toolbarPlugin = createToolbarPlugin({
+
+  theme: { buttonStyles, toolbarStyles },
+
+  structure: [
+    BoldButton,
+    ItalicButton,
+    UnderlineButton,
+    HeadlinesButton,
+    UnorderedListButton,
+    OrderedListButton,
+  ]
+});
+
+const { Toolbar } = toolbarPlugin;
 
 const imagePlugin = createImagePlugin();
 
@@ -159,25 +149,35 @@ export default class HeadlineCreator extends Component {
   );
 
   definitionSearch = (Search('/api/real/content', (data) => {
-    this.setState({ definitions: [
-        { name: "ISIS", value : "The Islamic State of Iraq and the Levant (ISIL /ˈaɪsəl/), also known as the Islamic State of Iraq and Syria, the Islamic State of Iraq and al-Sham (ISIS /ˈaɪsɪs/)[55], officially known as the Islamic State (IS) and by its Arabic language acronym Daesh (Arabic: داعش‎ dāʿish, IPA: [ˈdaːʕɪʃ]),[56][57] is a Salafi jihadist terrorist organisation and former unrecognised proto-state that follows a fundamentalist, Salafi/Wahhabi doctrine of Sunni Islam.[58][59]"},
-        { name: "Operations" , value : "By operations we mean blah blah blah" },
-        { name : "The Levant", value : "The Levant (/ləˈvænt/) is an approximate historical geographical term referring to a large area in the Eastern Mediterranean. In its narrowest sense, it is equivalent to the historical region of Syria. In its widest historical sense, the Levant included all of the eastern Mediterranean with its islands;[3] that is, it included all of the countries along the Eastern Mediterranean shores, extending from Greece to Cyrenaica.[2][4]" },
+    this.setState({
+      definitions: [
+        {
+          name: "ISIS",
+          value: "The Islamic State of Iraq and the Levant (ISIL /ˈaɪsəl/), also known as the Islamic State of Iraq and Syria, the Islamic State of Iraq and al-Sham (ISIS /ˈaɪsɪs/)[55], officially known as the Islamic State (IS) and by its Arabic language acronym Daesh (Arabic: داعش‎ dāʿish, IPA: [ˈdaːʕɪʃ]),[56][57] is a Salafi jihadist terrorist organisation and former unrecognised proto-state that follows a fundamentalist, Salafi/Wahhabi doctrine of Sunni Islam.[58][59]"
+        },
+        {name: "Operations", value: "By operations we mean blah blah blah"},
+        {
+          name: "The Levant",
+          value: "The Levant (/ləˈvænt/) is an approximate historical geographical term referring to a large area in the Eastern Mediterranean. In its narrowest sense, it is equivalent to the historical region of Syria. In its widest historical sense, the Levant included all of the eastern Mediterranean with its islands;[3] that is, it included all of the countries along the Eastern Mediterranean shores, extending from Greece to Cyrenaica.[2][4]"
+        },
 
       ],
     });
   }));
 
   graphSearch = (Search('/api/real/content', (data) => {
-    this.setState({ graphs: [
-        { name: "Social media report"},
-        { name: "Other graph" },
+    this.setState({
+      graphs: [
+        {name: "Social media report"},
+        {name: "Other graph"},
       ],
     });
   }));
 
   heatmapSearch = (Search('/api/real/content', (data) => {
-    this.setState({ heatmaps: [{ name : "Somewhere Bad", 'data' : 'heatmap_bad', 'colours' : [
+    this.setState({
+      heatmaps: [{
+        name: "Somewhere Bad", 'data': 'heatmap_bad', 'colours': [
           'interpolate',
           ['linear'],
           ['heatmap-density'],
@@ -186,7 +186,9 @@ export default class HeadlineCreator extends Component {
           0.4, 'rgb(166,189,219)',
           0.6, 'rgb(103,169,207)',
           0.8, 'rgb(28,144,153)'
-        ], geojson : {duration : 2000, center : [-79.94606,40.44961], zoom : 12}}, { name : "Somewhere Good",  'data' : 'heatmap_good', 'colours' : [
+        ], geojson: {duration: 2000, center: [-79.94606, 40.44961], zoom: 12}
+      }, {
+        name: "Somewhere Good", 'data': 'heatmap_good', 'colours': [
           'interpolate',
           ['linear'],
           ['heatmap-density'],
@@ -195,18 +197,21 @@ export default class HeadlineCreator extends Component {
           0.4, 'rgb(12,18,219)',
           0.6, 'rgb(12,19,207)',
           0.8, 'rgb(1,144,153)'
-        ], geojson : {duration : 2000, center : [-79.94606,40.44961], zoom : 12}}] });
+        ], geojson: {duration: 2000, center: [-79.94606, 40.44961], zoom: 12}
+      }]
+    });
   }));
 
   contentSearch = (Search('/api/real/content', (data) => {
-    this.setState({ suggestions: data.list });
+    this.setState({suggestions: data.list});
   }));
 
   regionSearch = (Search('/api/real/content', (data) => {
-    this.setState({ regions: [
-        { name: "North Pitsburgh",       geojson: {duration : 1000,  center: [-79.92606,40.34961], zoom: 12}},
-        { name: "South Pitchsburgh",       geojson: {duration : 1000,  center: [-79.87606,40.54961], zoom: 12} },
-        { name : "Somewhere Bad",   geojson : {duration : 2000, center : [-79.94606,40.44961], zoom : 12}}
+    this.setState({
+      regions: [
+        {name: "North Pitsburgh", geojson: {duration: 1000, center: [-79.92606, 40.34961], zoom: 12}},
+        {name: "South Pitchsburgh", geojson: {duration: 1000, center: [-79.87606, 40.54961], zoom: 12}},
+        {name: "Somewhere Bad", geojson: {duration: 2000, center: [-79.94606, 40.44961], zoom: 12}}
 
       ],
     });
@@ -216,9 +221,9 @@ export default class HeadlineCreator extends Component {
     editorState: EditorState.createWithContent(convertFromRaw(raw)),
     suggestions: [],
     regions: [],
-    heatmaps : [],
-    definitions : [],
-    graphs : [],
+    heatmaps: [],
+    definitions: [],
+    graphs: [],
   }
 
   onChange = (editorState) => {
@@ -229,71 +234,74 @@ export default class HeadlineCreator extends Component {
 
     return (
       <div>
-        <div className='editor'>
 
-          <Editor
-            editorState={this.state.editorState}
-            onChange={this.onChange}
-            plugins={[this.graphPlugin, this.definitionPlugin, this.contentPlugin, this.regionPlugin, this.heatmapPlugin,  imagePlugin]}
-            ref={(element) => {
-              this.editor = element
-            }}
-          />
+          <div className={editorStyles.editor} onClick={this.focus}>
 
-          <this.graphPlugin.MentionSuggestions
-            key="34"
-            entryComponent={GraphEntry}
-            onSearchChange={ this.graphSearch }
-            suggestions={ this.state.graphs}
-            onClose={() => this.setState({graphs: []})}
-          />
+            <Editor
+              editorState={this.state.editorState}
+              onChange={this.onChange}
+              plugins={[toolbarPlugin, this.graphPlugin, this.definitionPlugin, this.contentPlugin, this.regionPlugin, this.heatmapPlugin,  imagePlugin]}
+              ref={(element) => {
+                this.editor = element
+              }}
+            />
 
-          <this.definitionPlugin.MentionSuggestions
-            key="0"
-            entryComponent={DefinitionEntry}
-            onSearchChange={ this.definitionSearch }
-            suggestions={ this.state.definitions }
-            onClose={() => this.setState({definitions: []})}
-          />
+            <Toolbar />
 
-          <this.contentPlugin.MentionSuggestions
-            key="1"
-            entryComponent={ContentEntry}
-            onSearchChange={this.contentSearch}
-            suggestions={this.state.suggestions}
-            onClose={() => this.setState({suggestions: []})}
-          />
+            <this.graphPlugin.MentionSuggestions
+              key="34"
+              entryComponent={GraphEntry}
+              onSearchChange={this.graphSearch}
+              suggestions={this.state.graphs}
+              onClose={() => this.setState({graphs: []})}
+            />
 
-          <this.regionPlugin.MentionSuggestions
-            key="2"
-            entryComponent={RegionEntry}
-            onSearchChange={this.regionSearch}
-            suggestions={this.state.regions}
-            onClose={() => this.setState({regions: []})}
-          />
+            <this.definitionPlugin.MentionSuggestions
+              key="0"
+              entryComponent={DefinitionEntry}
+              onSearchChange={this.definitionSearch}
+              suggestions={this.state.definitions}
+              onClose={() => this.setState({definitions: []})}
+            />
 
-          <this.heatmapPlugin.MentionSuggestions
-            key="3"
-            entryComponent={HeatmapEntry}
-            onSearchChange={this.heatmapSearch}
-            suggestions={this.state.heatmaps}
-            onClose={() => this.setState({heatmaps: []})}
-          />
+            <this.contentPlugin.MentionSuggestions
+              key="1"
+              entryComponent={ContentEntry}
+              onSearchChange={this.contentSearch}
+              suggestions={this.state.suggestions}
+              onClose={() => this.setState({suggestions: []})}
+            />
 
-          <ImageAdd
-            editorState={this.state.editorState}
-            onChange={this.onChange}
-            modifier={imagePlugin.addImage}
-          />
+            <this.regionPlugin.MentionSuggestions
+              key="2"
+              entryComponent={RegionEntry}
+              onSearchChange={this.regionSearch}
+              suggestions={this.state.regions}
+              onClose={() => this.setState({regions: []})}
+            />
 
-        </div>
+            <this.heatmapPlugin.MentionSuggestions
+              key="3"
+              entryComponent={HeatmapEntry}
+              onSearchChange={this.heatmapSearch}
+              suggestions={this.state.heatmaps}
+              onClose={() => this.setState({heatmaps: []})}
+            />
+
+            {/*<ImageAdd
+              editorState={this.state.editorState}
+              onChange={this.onChange}
+              modifier={imagePlugin.addImage}
+            />*/}
+
+          </div>
 
         <ul>
-          <li>@H - graph </li>
+          <li>@H - graph</li>
           <li>@D - definition</li>
-          <li>@G</li>
-          <li>^</li>
-          <li>@R</li>
+          <li>@G - graph</li>
+          <li>^ - content</li>
+          <li>@R - region</li>
         </ul>
 
       </div>
